b311ffb6faaa6dc2733688f3300da989741dd0f1
Nishika Singh <nishikasingh0307@gmail.com>
2026-07-19 22:34:11 +0530

    Update client.js

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const client = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+console.log("API URL is:", import.meta.env.VITE_API_URL);
 
 client.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
